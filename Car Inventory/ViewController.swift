b636ef604ecfe5a9083fb6d0b3139ad6dc89d374
Arthur Ataide <arthurreisataide@gmail.com>
2020-03-28 11:57:18 -0400

    Initial Commit

---
+++ Car Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car Inventory
+//
+//  Created by Arthur Ataide on 2020-03-28.
+//  Copyright © 2020 Arthur Ataide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
